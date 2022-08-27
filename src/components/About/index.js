bb9cec9ab1feb5135dd9560b51ca46ed16138e6c
denise-alvarez <denisealvarez3@gmail.com>
2022-08-26 21:07:33 -0700

    update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,10 +8,7 @@ function About() {
         <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div>
         <p>
-        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch Etsy authentic Marfa quinoa typewriter plaid direct trade small batch wayfarers bicycle rights cliche craft beer gastropub single-origin coffee Godard Carles you probably haven't heard of them irony pickled kitsch synth sriracha gentrify literally heirloom blog Truffaut paleo scenester
-        </p>
-        <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
+        Hello everyone! I'm Denise Alvarez, based out of Bakersfield, CA. I have my Bachelors is Business Management. I have recently completed a full stack bootcampt at UCI. Please take a look around at everything I have done. Feel free to contact me if you have any questions!!
         </p>
       </div>
     </section>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,15 +6,15 @@ function Footer() {
   const icons = [
     {
       name: "fab fa-github",
-      link: "https://github.com/"
+      link: "https://github.com/denise-alvarez"
     },
     {
       name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/"
+      link: "https://www.linkedin.com/in/denise-alvarez-45bb48123/"
     },
     {
       name: "fab fa-stack-overflow",
-      link: "https://stackoverflow.com/"
+      link: "https://stackoverflow.com/users/18330343/denise-alvarez"
     }
   ]
 
